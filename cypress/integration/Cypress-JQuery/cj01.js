ffde731d1d666c94c166fb576d1ba35dd71273cb
smajumdar927 <58828730+smajumdar927@users.noreply.github.com>
2022-01-03 20:54:38 -0500

    added a text

--- cypress/integration/Cypress-JQuery/cj01.js
+++ cypress/integration/Cypress-JQuery/cj01.js
@@ -15,4 +15,12 @@ describe("Cypress JQuery Suite", ()=>{
             expect(str).to.equal("Hello , Are you sure you want to confirm?")
         })
     });
+    it("Remove the attribute from the DOM so that the page doesn't open in the new tab", () => {
+        cy.get("#opentab").invoke("removeAttr", "target").click()
+        cy.url().should("include", "rahulshettyacademy") //validate the url of the website
+        cy.go("back")
+    });
+    it.skip("Remove the attribute from the DOM so that the page doesn't open new window", ()=>{
+        cy.get("#openwindow").invoke("removeAttr", "onclick").click() //cypress doesn't handle window handle
+    })
 })
